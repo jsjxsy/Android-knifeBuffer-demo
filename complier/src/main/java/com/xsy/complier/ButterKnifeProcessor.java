c96cac3d2db3896643dbce7ef2b89b637e69f859
jsjxsy <jsjxsy@gmail.com>
2019-10-19 18:50:45 +0800

    模仿 bufferKnife 的实现

---
+++ complier/src/main/java/com/xsy/complier/ButterKnifeProcessor.java
@@ -0,0 +1,55 @@
+package com.xsy.complier;
+
+import com.xsy.annotation.BindView;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Completion;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+
+/**
+ * @author xiaosy
+ * @create 2019-05-21
+ * @Describe
+ **/
+public class ButterKnifeProcessor extends AbstractProcessor {
+    @Override
+    public Set<String> getSupportedOptions() {
+        return super.getSupportedOptions();
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> annotations = new LinkedHashSet<>();
+        annotations.add(BindView.class.getName());
+        return super.getSupportedAnnotationTypes();
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.RELEASE_7;
+    }
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+
+    @Override
+    public Iterable<? extends Completion> getCompletions(Element element, AnnotationMirror annotation, ExecutableElement member, String userText) {
+        return super.getCompletions(element, annotation, member, userText);
+    }
+}
